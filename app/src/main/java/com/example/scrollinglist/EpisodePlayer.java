44c020386565f8d9d7443dc4bf7f7452ee7448e2
Belanger <belangek@c02qqacdg8wp.ad.viacom.com>
2016-09-27 09:59:54 -0400

    added video persistence

--- app/src/main/java/com/example/scrollinglist/EpisodePlayer.java
+++ app/src/main/java/com/example/scrollinglist/EpisodePlayer.java
@@ -1,15 +1,28 @@
 package com.example.scrollinglist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
+import com.vmn.android.freewheel.FreewheelPluginController;
+import com.vmn.android.freewheel.impl.FWAdSlot;
+import com.vmn.android.freewheel.impl.FreewheelPlugin;
 import com.vmn.android.player.AndroidPlayerContext;
 import com.vmn.android.player.VMNVideoPlayerImpl;
+import com.vmn.android.player.api.PreparedContentItem;
 import com.vmn.android.player.model.MGID;
 import com.vmn.android.player.model.VMNContentSession;
 import com.vmn.android.player.view.VideoSurfaceView;
+import com.vmn.functional.Optional;
+
+import java.net.URI;
+
+import tv.freewheel.ad.interfaces.IAdInstance;
 
 public class EpisodePlayer extends AppCompatActivity {
     private static final String TAG = EpisodePlayer.class.getSimpleName();
@@ -18,6 +31,8 @@ public class EpisodePlayer extends AppCompatActivity {
     private String appName;
     private static final int MAX_BUFFER_SIZE_MEGS = 32;
     static final String MGID_STRING = "mgid_string";
+    @NonNull
+    private SingletonPlayer singleton;
     public static final String PLAYER_SESSION_KEY = "playerSession"; /*Optional: used to
     serialize and save the session to prevent reloading if the activity restarts such as
     due to a screen rotation*/
@@ -25,12 +40,13 @@ public class EpisodePlayer extends AppCompatActivity {
     private VideoSurfaceView videoPlayerView;
     private AndroidPlayerContext playerContext;
     private VMNVideoPlayerImpl videoPlayer;
+    private VMNContentSession session = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.episode_player);
-
+        videoPlayerView = (VideoSurfaceView)findViewById(R.id.video_player);
 
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
@@ -50,15 +66,9 @@ public class EpisodePlayer extends AppCompatActivity {
 
 
 
-        VMNContentSession session = null;
-        playerContext = new AndroidPlayerContext(
-                this.getApplication(),
-                "demoAdvertisingId");
-        //this.getClass().getInstance().getTveService().getAuthBridge(),
-        //MAX_BUFFER_SIZE_MEGS);
-        //session = playerContext.buildSession(episodeMGID, appName,
-        //       false).authRequired(false).build(); //authRequired is false because we are only
-        //viewing the free clips, as opposed to those requiring an account
+
+        singleton = SingletonPlayer.getInstance();
+        playerContext = singleton.getPlayerContext();
 
         videoPlayer = playerContext.buildPlayer()
                 .autoPlay(true) // true iff each loaded video should start playing immediately regardless of prior player state
@@ -72,7 +82,51 @@ public class EpisodePlayer extends AppCompatActivity {
                 false).authRequired(false).build(); //authRequired is false because we are only
         //viewing the free clips, as opposed to those requiring an account
         videoPlayer.clear();
-        videoPlayer.enqueue(playerContext.prepareSession(session));
+        videoPlayer.enqueue(playerContext.prepareSession(session)); // Use the Freewheel plugin to receive ad clickthroughs.
+
+
+        final Optional<FreewheelPlugin> freewheelPlugin = playerContext.findPlugin(FreewheelPlugin.class);
+        if (freewheelPlugin.isPresent()) {
+            final Optional<FreewheelPluginController> freewheelPluginController = freewheelPlugin.get().interfaceFor(videoPlayer);
+            freewheelPluginController.get().registerDelegate(new FreewheelPluginController.DelegateBase() {
+                @Override
+                public void instanceClickthroughTriggered(FWAdSlot slot, IAdInstance instance, URI uri) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(uri.toString())));
+                }
+            });
+        }
+
+
+        // Provide a reference to the base view that contains all the player visual components.  The player and its
+        // various plugins will look for specific IDs within this view, and hook them up to specific functional elements.
+        // The player wants to draw into an instance of VMNPlayerView with ID R.id.video_player, and so on.
+        // Check res/layout/demo_player.xml for an example of how a typical player layout will work.
+        videoPlayer.setView(Optional.<View>of(videoPlayerView));
+
+        // Collect the session that is to be started from either the bundle or the Intent (the latter comes from
+        // DemoSelector.java; the former from `onSaveInstanceState` of prior versions of this Activity).
+        if (savedInstanceState != null) session = (VMNContentSession) savedInstanceState.getSerializable(PLAYER_SESSION_KEY);
+        if (session == null) session = (VMNContentSession)getIntent().getSerializableExtra(PLAYER_SESSION_KEY);
+        if (session == null) {
+            Log.e("DemoPlayerActivity", "Failed to load session from Bundle or Intent");
+            finish();
+        }
+
+        // Check the shared location for an already-prepared item (see DemoSingleton.java).
+        final Optional<PreparedContentItem> preparedItem = singleton.getPreparedItem(session);
+
+        // If we had a prepared item, pass it into the player; if not, prepare a fresh one using the given session.
+        videoPlayer.enqueue(preparedItem.isPresent()
+                ? preparedItem.get()
+                : playerContext.prepareSession(session));
+
+        // Check this class for some specific enhancements and custom ways of using the player.
+        //final DemoExtensions demoExtensions = new DemoExtensions(videoPlayer);
+
+        // At this point, your video should be playing.  Any time spent by the user between looking at the selector
+        // screen and loading a video will be used to prepare content, so video playback should be quite responsive.
+        // Note however that we are contractually prevented from preparing Freewheel content in advance, so if prerolls
+        // are in use there will be a minimum startup delay defined by the response time of the preroll slot.
     }
 
     @Override
@@ -115,6 +169,7 @@ public class EpisodePlayer extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         //Log.d(TAG, "onRestoreInstanceState: " + mStringMGID);
         outState.putString(MGID_STRING, mStringMGID);
+        outState.putSerializable(PLAYER_SESSION_KEY, session);
         super.onSaveInstanceState(outState);
     }
 
---
+++ app/src/main/java/com/example/scrollinglist/SingletonPlayer.java
@@ -0,0 +1,87 @@
+package com.example.scrollinglist;
+
+import com.example.scrollinglist.video.DemoPlugin;
+import com.vmn.android.freewheel.impl.FreewheelPlugin;
+import com.vmn.android.player.AndroidPlayerContext;
+import com.vmn.android.player.api.PreparedContentItem;
+import com.vmn.android.player.controls.MediaControlsPlugin;
+import com.vmn.android.player.model.VMNContentSession;
+import com.vmn.android.player.plugin.bento.BentoPlugin;
+import com.vmn.android.player.plugin.captions.CaptionsPlugin;
+import com.vmn.functional.Optional;
+import com.vmn.util.ErrorHandler;
+import com.vmn.util.PlayerException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by belangek on 9/26/16.
+ */
+
+public class SingletonPlayer {
+    private static final com.example.scrollinglist.SingletonPlayer instance = new com.example.scrollinglist.SingletonPlayer();
+    public static com.example.scrollinglist.SingletonPlayer getInstance() { return instance; }
+
+
+    // Define a location to store prepared items. The session makes a good key for this map.
+    private final Map<VMNContentSession, PreparedContentItem> preparedContent = new HashMap<>();
+
+
+    public void addPreparedItem(PreparedContentItem item) {
+        preparedContent.put(item.getSession(), item);
+    }
+
+    // Define an operation to retrieve prepared content.  It's a good idea to throw away the prepared item after it
+    // is returned, because it is usually only practical to use it once (the prepared item 'remembers' failures, so
+    // future attempts to load a failed prepared item will instantly fail).
+    public Optional<PreparedContentItem> getPreparedItem(VMNContentSession session) {
+        try {
+            return Optional.from(preparedContent, session);
+        } finally {
+            preparedContent.remove(session); // A prepared item is only eligible to be used once
+        }
+    }
+
+    public void clearPreparedItems() { preparedContent.clear(); }
+
+    private static final int MAX_BUFFER_SIZE_MEGS = 32;
+
+    // Define a player context that provides the necessary bindings into application/Google services
+    private final AndroidPlayerContext androidPlayerContext = new AndroidPlayerContext(
+            MainApplication.getInstance(),
+            "demoAdvertisingId");//,
+    //MainApplication.getInstance().getTveService().getAuthBridge(),
+    //MAX_BUFFER_SIZE_MEGS);
+
+    public AndroidPlayerContext getPlayerContext() { return androidPlayerContext; }
+
+    // Turn this flag ON to skip the preroll and turn it off see the preroll ad playback. This flag is added for preroll optimization
+    // so if 'skipPreroll' is true and it's the first clip, ad context will still be fetched but beforeClip will not be blocked.
+    // If above condition is false, beforeClip will be blocked until
+    private final boolean skipPreroll = true;
+
+    // For each optional player module you would like to use, invoke bindPlugin() on it, passing in the appropriate
+    // configuration options to each.
+    private SingletonPlayer() {
+        // Any time a trapped exception happens through the player, notify Crashlytics.
+        androidPlayerContext.getErrorHandler().registerDelegate(new ErrorHandler.Delegate() {
+            @Override
+            public void exceptionOccurred(PlayerException exception) {
+                //Crashlytics.logException(exception);
+            }
+        });
+
+
+        // Link all plugins that each player instance will use
+        CaptionsPlugin.bindPlugin(androidPlayerContext);
+        FreewheelPlugin.bindPlugin(androidPlayerContext,
+                FreewheelPlugin.HandleClicks.APPLICATION, skipPreroll);
+        MediaControlsPlugin.bindPlugin(androidPlayerContext);
+        BentoPlugin.bindPlugin(androidPlayerContext);
+
+        // Check the source for this plugin to see detailed information on how to create a plugin.
+        DemoPlugin.bindPlugin(androidPlayerContext);
+    }
+}
